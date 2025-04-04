6aa55ebd5115efac8b03ca5c076d802c68751270
adtpdn <adtpdn@gmail.com>
2025-04-04 17:14:36 +0800

    update webdock CI

---
+++ ecosystem.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    apps: [{
+      name: 'mail-checker',
+      script: 'server.js',
+      instances: 'max',
+      exec_mode: 'cluster',
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '300M',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3000
+      }
+    }]
+  };
